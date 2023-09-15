58d1ef32d18a75230ee414c49bce9140a520bb87
Marcelo Welter <marcelo@alowelter.com.br>
2023-09-15 13:56:18 -0300

    ...

--- src/app.js
+++ src/app.js
@@ -11,6 +11,10 @@ const mysql = require('mysql2');
 const database = mysql.createConnection(process.env.DATABASE_URL);
 console.log('🟢 Mysql - PlanetScale');
 
+console.log('🟢 Sistema Operacional', os.platform());
+
+return;
+
 const proxy = httpProxy.createProxyServer({});
 const targets = [];
 
