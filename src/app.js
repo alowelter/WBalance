3754c00b783968806978ef100010e271c6b4a866
Marcelo Welter <marcelo@alowelter.com.br>
2023-09-17 15:35:16 -0300

    ...

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const useHttp = require('http');
-const useHttps = require('https');
-const httpProxy = require('http-proxy');
+const useProxy = require('http-proxy');
 const fs = require('fs');
 const os = require('os');
 
@@ -18,48 +17,70 @@ const mysql = require('mysql2');
 const database = mysql.createConnection(process.env.DATABASE_URL);
 console.log('🟢 Mysql - PlanetScale');
 
+// handles
+let instances = null;
+let loadbalance = null;
+
 if (os.platform() != 'linux') {
     console.log('🔴 Sistema deve ser linux');
     return;
 }
 
 // WebServer
-if (fs.existsSync(`../privkey.pem`)) {
+if (fs.existsSync(`privkey.pem`)) {
     const https = useHttps.createServer(
         {
-            key: fs.readFileSync(`../privkey.pem`),
-            cert: fs.readFileSync(`../fullchain.pem`),
+            key: fs.readFileSync(`/etc/letsencrypt/live/${process.env.BASEURL}/privkey.pem`),
+            cert: fs.readFileSync(`/etc/letsencrypt/live/${process.env.BASEURL}/fullchain.pem`),
         },
         app
     );
-    https.listen(8443, () => {
+    https.listen(443, () => {
         console.log('🟢 HTTPS Running - Port 8443');
     });
 } else {
-    console.log('🔴 HTTPS naõ encontrado certificado');
-
+    console.log('🔴 HTTPS - Não encontrado certificado');
+    console.log('🟡 tentando gerar');
+    const { exec } = require('child_process');
+    const comandoCertbot = `certbot certonly --standalone --email marcelo@alolwelter.com.br --agree-tos -d ${process.env.BASEURL}`;
+    exec(comandoCertbot, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Erro ao executar o comando: ${error}`);
+            process.exit(1);
+        }
+        console.log(`🔶 Resultado: ${stdout}`);
+        console.error(`🔴 Erros do comando: ${stderr}`);
+    });
     process.exit(1);
 }
 
 const http = useHttp.createServer(app);
-http.listen(8080, () => {
+http.listen(3001, () => {
     console.log('🟢 HTTP Running - Port 8080');
 });
 
 // Proxy
-const proxy = httpProxy.createProxyServer({});
+const proxy = useProxy.createProxyServer({});
 const targets = [];
 
-//console.log('Prefix', process.env.VULTR_SERVER_LABEL_PREFIX);
+app.use(async (req, res, next) => {
+    if (instances.length <= 0) {
+        res.status(500).send('Nenhum servidor disponível');
+        return;
+    }
+    console.log('instance ', instances[0]);
+
+    //proxy.web(req, res, { target: 'http://localhost:3001' }, (err) => {
+});
 
 async function main() {
     try {
         const instancesResponse = await api.instances();
-        const instances = instancesResponse.data;
+        instances = instancesResponse.data;
         console.log('instances --->', instances);
 
         const loadbalanceResponse = await api.loadbalance();
-        const loadbalance = loadbalanceResponse.data;
+        loadbalance = loadbalanceResponse.data;
         console.log('loadbalance --->', loadbalance);
 
         if (instances.length > 0) {
