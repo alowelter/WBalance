7598cba27404219e35326545c6e1752e81368a20
Marcelo Welter <marcelo@alowelter.com.br>
2023-09-18 12:23:31 -0300

    ...

--- src/controllers/InstancesController.js
+++ src/controllers/InstancesController.js
@@ -1,21 +1,21 @@
 const api = require('./ApiController');
 
-exports.Instances = async (req, res) => {
+exports.Instances = async () => {
     api.Get('/instances')
         .then((response) => {
-            res.json(response.data);
+            return response.data;
         })
         .catch((error) => {
-            res.json(error);
+            return error;
         });
 };
 
 exports.LoadBalance = async (req, res) => {
     api.Get('/instances')
         .then((response) => {
-            res.json(response.data);
+            return response.data;
         })
         .catch((error) => {
-            res.json(error);
+            return error;
         });
 };
